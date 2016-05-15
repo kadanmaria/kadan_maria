a2c18a4804baad81fdbd90c4d75b8376b6d177a2
Maria Kadan <kadan.maria@gmail.com>
2016-05-15 16:13:26 +0300

    First UI Updates

---
+++ Kadan/Kadan/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kadan
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MusicManager musicManager;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            MusicManager musicManager = new MusicManager();
+            musicManager.RegisterDelegate(new MusicManager.MusicManagerDelegate(showMessage));  
+            this.musicManager = musicManager;
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBox.Text != @"")
+            {
+                musicManager.getAllAudioFromFolderWithPath(textBox.Text);
+                textBox.Text = @"";
+            }
+        }
+
+        private void showMessage(String message)
+        {
+            label.Content = message;
+            Console.WriteLine(message);
+        }
+
+    }
+}
---
+++ Kadan/Kadan/MusicManager.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using TagLib;
+
+namespace Kadan
+{
+    class MusicManager
+    {
+        public delegate void MusicManagerDelegate(string message);
+        MusicManagerDelegate myDelegate;
+        private int count { get; set; }
+
+        public void RegisterDelegate(MusicManagerDelegate myDelegate)
+        {
+            this.myDelegate = myDelegate;
+        }
+
+        public MusicManager() {
+        }
+
+        public void getAllAudioFromFolderWithPath(string folderPath)
+        { 
+            DirectoryInfo dir = new DirectoryInfo(folderPath);
+
+            if (dir.Exists)
+            {
+                foreach (FileInfo file in dir.GetFiles("*.mp3"))
+                {
+                    var audioFile = TagLib.File.Create(file.FullName);
+                    Console.WriteLine("Album: {0}\n\n", audioFile.Tag.Album);
+                    count++;
+                }
+                if (count == 0 && myDelegate != null)
+                {
+                    myDelegate("There are no files in this directory. Try another one please!");
+                }
+                else if (count != 0 && myDelegate != null) {
+                    myDelegate(folderPath);
+                }
+            }
+            else {
+                if (myDelegate != null)
+                    myDelegate("There is no such directory. Try again please!");
+            }
+
+        }
+        
+        public void wrileInfoFromFile(String path) {
+            var audioFile = TagLib.File.Create(path);
+
+            Console.WriteLine("Album: {0}\nSinger: {1}\nTitle: {2}\nYear: {3}\nDuration: {4}"
+                , audioFile.Tag.Album
+                , String.Join(", ", audioFile.Tag.Performers)
+                , audioFile.Tag.Title
+                , audioFile.Tag.Year
+                , audioFile.Properties.Duration.ToString("mm\\:ss"));
+        }
+
+        
+    }
+}
